eb4a610574e220535b1a5572b9f18fbab94c2817
Walden Raines <walden@unchained-capital.com>
2020-05-06 14:28:29 -0400

    Updates

---
+++ precache-manifest.d6a7686315a18eeed7e7af02a07d7fb9.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a9c14195a5fedd7955a294e989a23eb4",
+    "url": "/caravan/index.html"
+  },
+  {
+    "revision": "b09a560939f713217d10",
+    "url": "/caravan/static/css/2.599c45ea.chunk.css"
+  },
+  {
+    "revision": "8bc14b9e44d163cea400",
+    "url": "/caravan/static/css/main.fcc2762e.chunk.css"
+  },
+  {
+    "revision": "b09a560939f713217d10",
+    "url": "/caravan/static/js/2.06c04128.chunk.js"
+  },
+  {
+    "revision": "f82985dd5e4c018b5d2dc4c18e05df3e",
+    "url": "/caravan/static/js/2.06c04128.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8bc14b9e44d163cea400",
+    "url": "/caravan/static/js/main.120509e6.chunk.js"
+  },
+  {
+    "revision": "2a5766c46f917ee670d6",
+    "url": "/caravan/static/js/runtime-main.01b265b8.js"
+  },
+  {
+    "revision": "66481e66e2bea46e7b7d73df9d4953d9",
+    "url": "/caravan/static/media/logo.66481e66.png"
+  }
+]);
