c8912b2cb7dae3c553c960d1e3d2b4d0e1a1ac96
Walden Raines <walden@unchained-capital.com>
2020-12-17 14:39:06 -0500

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/caravan/precache-manifest.efd50f291e960080adc92008854ead49.js"
+  "/caravan/precache-manifest.a72c6a217662799189cda947aad3bf8d.js"
 );
 
 self.addEventListener('message', (event) => {
